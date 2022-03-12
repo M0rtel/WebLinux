bab4a3397b497a32e3cdde27da55a5789e85b7a5
Mortel <igor.gorobets2013@yandex.ru>
2022-03-12 20:48:06 +0300

    Fourth commit

--- myProjects/urls.py
+++ myProjects/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.my_projects, name='my_projects'),
+    path('<int:pk>', views.ProjectsDetailView.as_view(), name='projects-detail') #когда мы обращаемся к классу нужно добавлять as_view()
 ]
--- myProjects/views.py
+++ myProjects/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+from .models import Projects
+from django.views.generic import DetailView
 
 def my_projects(request):
-    return render(request, 'myProjects/my_projects.html')
+    projects = Projects.objects.order_by('title')                                  # Первый способ получений данный через функцию
+    return render(request, 'myProjects/my_projects.html', {'projects': projects})
+
+
+class ProjectsDetailView(DetailView):                                              # Второй способ получений данный через класс
+    model = Projects
+    template_name = 'myProjects/details_view.html'
+    context_object_name = 'projects'
 
